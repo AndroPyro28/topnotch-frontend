9d888a59ac2b07d16ff71fd7f5d094e506d5bbcb
Andropyro28 <menandroeugenio1028@gmail.com>
2022-09-25 22:29:54 +0800

    'updating'

--- src/components/activities/OrderActivities.jsx
+++ src/components/activities/OrderActivities.jsx
@@ -52,9 +52,7 @@ function OrderActivities() {
           <Activity status={order.order_status}>
             <span class="service">
               Amount:{" "}
-              {ProductPriceFormmater(
-                order.total_amount + order.total_amount * 0.01
-              )}
+              {ProductPriceFormmater(order.total_amount)}
             </span>
           </Activity>
 
