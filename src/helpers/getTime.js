3087f69b9292f872282edaeb3435bbbea2434cdf
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-01 01:19:58 +0800

    'updating'

--- src/helpers/getTime.js
+++ src/helpers/getTime.js
@@ -12,7 +12,7 @@ import React from 'react'
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
         minutes = minutes < 10 ? '0'+minutes : minutes;
-        let strTime = hours + ':' + minutes + ':00';
+        let strTime = hours < 10 ? `0${hours}` : hours + ':' + minutes + ':00';
         return strTime;
     }
 
