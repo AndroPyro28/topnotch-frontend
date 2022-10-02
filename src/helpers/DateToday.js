c040780959a5e211e765318bc55afd05cf04ecbd
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-02 18:32:07 +0800

    'updating'

--- src/helpers/DateToday.js
+++ src/helpers/DateToday.js
@@ -2,4 +2,6 @@ const GetDateToday = () => {
     return new Date().toISOString().slice(0, 10);
 }
 
+
+
 export default GetDateToday
--- src/pages/customerPages/profile/Personal.jsx
+++ src/pages/customerPages/profile/Personal.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { UserInfo, RowInfo } from "./Personal";
 import { useSelector } from "react-redux";
 import { useOutletContext } from "react-router-dom";
+import GetDateToday from "../../../helpers/DateToday";
+import AppointmentLogic from "../appointment/appointmentLogic";
 function Personal() {
   const {allowChanges, setUser, user,} = useOutletContext();
   const { currentUser } = useSelector((state) => state.user);
   
-
+const {dateTodayFormatter} = AppointmentLogic({})
   useEffect(() => {
     setUser(currentUser);
   }, [currentUser]);
@@ -37,14 +39,14 @@ function Personal() {
 
         <div class="info">
           <h3>CONTACT</h3>
-          {allowChanges ? <input value={user?.phoneNo} name="phoneNo" onChange={setProps} /> : <span>{`${user?.phoneNo}`}</span>}
+          {allowChanges ? <input type="number" value={user?.phoneNo} name="phoneNo" onChange={setProps} /> : <span>{`${user?.phoneNo}`}</span>}
         </div>
       </RowInfo>
 
       <RowInfo>
         <div class="info">
           <h3>DATE OF BIRTH</h3>
-          {allowChanges ? <input type="date" value={user?.birthdate} name="birthdate" onChange={setProps} /> : <span>{`${user?.birthdate}`}</span>}
+          {allowChanges ? <input type="date" min={dateTodayFormatter({})} value={user?.birthdate} name="birthdate" onChange={setProps} /> : <span>{`${user?.birthdate}`}</span>}
         </div>
 
         <div class="info">
